1bfb8c551210e47be69240b5d7957843d6c3c714
Java Full Stack Developer Hamit Mızrak <hamitmizrak@gmail.com>
2022-05-19 19:35:42 +0300

    Update JavaCompiler.java

--- JavaCompiler.java
+++ JavaCompiler.java
@@ -1,6 +1,6 @@
 
 public class JavaCompiler{
 	public static void main(String [] args){
-		System.out.println("Merhabalar Java Compiler ");
+		System.out.println("Merhabalar Java Compiler 44 ");
 	}
 }
